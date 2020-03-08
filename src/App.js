a6424b5bd03000893aadf79fcf87ceb590041f9e
neotamoe <neota.moe@daugherty.com>
2020-03-07 20:53:25 -0600

    initial commit including foodcard, heading, signup, readme/todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FoodCard from './components/FoodCard'
+import Heading from './components/Heading'
+import SignUp from './components/SignUp'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Heading title="Augsburg Food Call"/>
       </header>
+      <div>
+        <FoodCard name="Penne Pasta"/>
+        <FoodCard name="Cookies"/>
+        <FoodCard name="Sandwiches"/>
+        <FoodCard name="Ice Cream"/>
+      </div>
+      <div>
+        <SignUp />
+      </div>
     </div>
   );
 }
---
+++ src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import Heading from './Heading'
+
+const SignUp = () => {
+  return (
+    <div>
+      <Heading title="Sign Up"/>
+    </div>  
+  )
+}
+
+export default SignUp
