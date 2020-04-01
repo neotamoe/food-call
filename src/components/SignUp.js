5d4f82fff0910519fb79bf714e51910433b568e7
neotamoe <neota.moe@gmail.com>
2020-03-31 19:19:49 -0500

    implemented requirement to check at least one box

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -19,17 +19,17 @@ const StyledDiv = styled.div`
 
 const SignUp = props => {
   const [isSubmitting, setIsSubmitting] = useState(false)
-  const { register, handleSubmit, errors } = useForm()
-  const onFormSubmit = (data, e) => {
-    console.log(typeof e.target[0].checked); 
-    console.log(typeof e.target[1].checked); 
-    console.log('type of data.post: '+typeof data.post); 
-    console.log('typeof data.receive '+ typeof data.receive); 
+  const { register, handleSubmit, errors, getValues } = useForm()
+
+  const onFormSubmit = data => {
     console.log(data)
     setIsSubmitting(true)
-  }
-  
-    return (
+  }  
+  const validate = () => {
+    return ["post", "receive"].some(name => getValues()[name]);
+  };
+
+  return (
     <Grid item={true} xs={12} sm={6}>
       <Card>
         <form onSubmit={handleSubmit(onFormSubmit)}>
@@ -37,7 +37,7 @@ const SignUp = props => {
           <p>Sign up to post a Food Call or receive emails when new food calls are posted.</p>
           <FormGroup>
             <Input id="email" name="email" type="email" placeholder="Enter your email" inputRef={register({required: true})} />
-            {errors.email && <InputError>This field is required</InputError>}
+            {errors.email && errors.email.type === 'required' && <InputError>This field is required</InputError>}
           </FormGroup>
           <StyledDiv>
             <FormGroup>
@@ -50,7 +50,10 @@ const SignUp = props => {
                   />}
                 label="I want to post..."
                 labelPlacement="end"
-                inputRef={register}
+                inputRef={register({
+                  onebox: 'You must check at least one checkbox!',
+                  validate: validate,
+                })}
               />
             </FormGroup>
             <FormGroup>
@@ -63,9 +66,16 @@ const SignUp = props => {
                   />}
                 label="I want to receive email notifications..."
                 labelPlacement="end"
-                inputRef={register}
+                inputRef={register({
+                  onebox: 'You must check at least one checkbox!',
+                  validate: validate,
+                })}
               />
             </FormGroup>
+              { 
+                (errors.post && <InputError>You must check at least one box</InputError>) ||
+                (errors.receive && <InputError>You must check at least one box</InputError>)
+              }
           </StyledDiv>
           <FormGroup>
             <Button variant="contained" color="primary" type="submit">
